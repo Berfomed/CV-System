c32443aee652cd8076f63a2f1a005bf9d9aeab88
MichaelSt9 <msaa1003jm@gmail.com>
2018-06-20 19:12:52 -0500

    Ajustes de catálogo - Cliente

--- src/java/edu/cvsystem/beans/ProductosCatalogoBean.java
+++ src/java/edu/cvsystem/beans/ProductosCatalogoBean.java
@@ -1,69 +1,45 @@
 package edu.cvsystem.beans;
 
 import edu.cvsystem.entidades.ProductoInventario;
-import edu.cvsystem.facades.ProductoInventarioFacade;
+import edu.cvsystem.entidades.Productos;
+import edu.cvsystem.facades.ProductosFacade;
 import javax.inject.Named;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 @Named(value = "productosCatalogoBean")
 @RequestScoped
 public class ProductosCatalogoBean {
 
-    @EJB
-    private ProductoInventarioFacade productoFacade;
+  @EJB
+  private ProductosFacade productoFacade;
 
-    public ProductosCatalogoBean() {
-    }
+  public ProductosCatalogoBean() {
+  }
 
-    @PostConstruct
-    public void init() {
-        producto = new ProductoInventario();
-        filtro = "";
-    }
+  @PostConstruct
+  public void init() {
+    producto = new ProductoInventario();
+  }
 
-    public List<ProductoInventario> listarProductos() {
-        List<ProductoInventario> lista = productoFacade.findAll();
-        if (!filtro.equals("")) {
-            for (int i = 0; i < lista.size(); i++) {
-                if (!lista.get(i).getIdProducto().getNombre().equalsIgnoreCase(filtro)) {
-                    System.out.println(lista.get(i).getIdProducto().getNombre());
-                    lista.remove(i);
-                }
-            }
-        }
-        return lista;
-    }
-    
-//    public List<ProductoInventario> listarProductos() {
-//        String query = "SELECT p FROM ProductoInventario p WHERE p.estado = 'disponible' ORDER BY p.fechaIngreso DESC";
-//        Query consulta = productoFacade.getEm().createQuery(query);
-//
-//        consulta.setParameter("filtro", "");
-//
-//        List<ProductoInventario> lista = consulta.getResultList();
-//        return lista;
-//    }
+  public List<Productos> listarProductos() {
+    TypedQuery<Productos> query = productoFacade.getEm()
+            .createQuery("SELECT p FROM Productos p WHERE p.estatus = :estatus",
+                    Productos.class);
+    query.setParameter("estatus", "Disponible");
+    return query.getResultList();
+  }
 
-    public ProductoInventario getProducto() {
-        return producto;
-    }
+  public ProductoInventario getProducto() {
+    return producto;
+  }
 
-    public void setProducto(ProductoInventario producto) {
-        this.producto = producto;
-    }
+  public void setProducto(ProductoInventario producto) {
+    this.producto = producto;
+  }
 
-    public String getFiltro() {
-        return filtro;
-    }
-
-    public void setFiltro(String filtro) {
-        this.filtro = filtro;
-    }
-
-    private ProductoInventario producto;
-    private String filtro;
+  private ProductoInventario producto;
 }
